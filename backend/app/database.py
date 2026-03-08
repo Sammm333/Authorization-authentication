f3f7af8f9cdc5f4bbeaa66c823b299c3e3ddaa3a
Khrosh771 <samvelkhachatryan771@gmail.com>
2026-03-08 23:12:40 +0400

    feat: Megrate PostgresSQL

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,17 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_DATABASE_URL = "sqlite:///./users.db"
+load_dotenv()
 
-engine = create_engine(SQL_DATABASE_URL, connect_args=({"check_same_thread": False}))
+SQL_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./users.db")
+
+if SQL_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQL_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQL_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
